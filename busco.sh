62a6f542f7e7c67a0fabaecc3a94d9510e5399a8
Victoria K Eggers <86014601+vkeggers@users.noreply.github.com>
2024-09-23 03:47:38 -0400

    Create busco.sh

---
+++ busco.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --account iacc_jfierst
+#SBATCH --qos highmem1
+#SBATCH --partition highmem1
+#SBATCH --output=out_%busco.log
+#SBATCH --mail-user=vegge003@fiu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -n 12
+
+module load busco/5.4.7
+
+export AUGUSTUS_CONFIG_PATH="/home/data/jfierst/veggers/programs/Augustus"
+max_jobs=12
+job_count=0
+
+while read -r line; do
+  busco -f -c 4 -m genome -i ./RhabditinaPhylogeny_NCBI/"${line}"/*.fna -o busco_"${line}" --offline --lineage_dataset /home/data/jfierst/veggers/nematoda_odb10 &
+
+    job_count=$((job_count + 1))
+
+        if [ "$job_count" -ge "$max_jobs" ]; then
+                wait
+                job_count=0
+        fi
+
+done < busco_list.txt
+
+
+#some of my files were gzipped and so they didn't work the first time. consequences of downloading genomes one by one instead of computationally. 
+#I made the following loop to run through the ones that needed to be fixed after I unzipped the files
+
+#while read -r line; do
+# busco -f -c 4 -m genome -i ./RhabditinaPhylogeny_NCBI/${line}/*.fna -o busco_${line} --offline --lineage_dataset /home/data/jfierst/veggers/nematoda_odb10
+#done < fix_list.txt
